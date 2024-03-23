5bd9878d328bddee818488f3d3386412a428777a
Artem Fedorov <s02210577@gse.cs.msu.ru>
2024-03-23 23:53:11 +0300

    adding new slider to control min size of cluster
    fix of centroid label bug

--- UI/tools/CountObjects.py
+++ UI/tools/CountObjects.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QCheckBox
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QCheckBox, QSlider
+from PyQt6.QtCore import QSize
+from PyQt6.QtCore import Qt
 
 
 class CountObjects(QWidget):
@@ -8,18 +10,37 @@ class CountObjects(QWidget):
 
         self.LABEL_START = 'Число объектов: '
 
-        layout = QHBoxLayout()
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        button_layout = QHBoxLayout()
 
         button_count = QPushButton('Найти объекты')
         button_count.clicked.connect(self.engine.find_componens)
-        layout.addWidget(button_count)
+        button_layout.addWidget(button_count)
 
         self.label_count_objects = QLabel()
         self.engine.on_change(self.change_object_count)
-        layout.addWidget(self.label_count_objects)
+        button_layout.addWidget(self.label_count_objects)
 
         self.checkbox = QCheckBox('показывать центры объектов')
         self.checkbox.stateChanged.connect(self.show_objects)
+
+        layout.addItem(button_layout)
+
+        clsize_layout = QHBoxLayout()
+        label_size = QLabel('минимальный размер: ')
+        min_clust_size_slider = QSlider(Qt.Orientation.Horizontal)
+        min_clust_size_slider.setRange(0, 100)
+        min_clust_size_slider.setValue(15)
+        min_clust_size_slider.setFixedSize(QSize(200, 30))
+        min_clust_size_slider.valueChanged.connect(
+            self.engine.change_min_clustsize)
+
+        clsize_layout.addWidget(label_size)
+        clsize_layout.addWidget(min_clust_size_slider)
+
+        layout.addItem(clsize_layout)
 
         self.setLayout(layout)
         self.change_object_count()
@@ -28,6 +49,7 @@ class CountObjects(QWidget):
         self.engine.on_upload(self.clearing)
 
     def change_object_count(self):
+
         count = self.engine.get_count()
         self.label_count_objects.setText(self.LABEL_START + str(count))
 
--- models.py
+++ models.py
@@ -7,4 +7,4 @@ def count_bounded_parts(grayscale_img) -> tuple[int, tuple[np.array, np.array]]:
     num_labels, labels, stats, centroids = \
         connectedComponentsWithStats(img_map, 8)
 
-    return num_labels - 1, (np.ravel(centroids[:, 1].astype(int)), np.ravel(centroids[:, 0].astype(int)))
+    return stats[1:, 4], (np.ravel(centroids[1:, 1].astype(int)), np.ravel(centroids[1:, 0].astype(int)))
